d9e6e8bcdd8ca4e516618be7426ea38cd9c7b6eb
Zmey56 <zmey56@gmail.com>
2022-09-20 16:44:32 +0400

    channel

---
+++ channels/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	fmt.Println(<-c)
+
+	duration := time.Since(start)
+	fmt.Println(duration)
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- "Yep it's up"
+}
